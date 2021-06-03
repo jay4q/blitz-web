60e3de9281c0275b986414a610465819296d9e83
jay4q <jay4qqqq@gmail.com>
2021-06-03 12:01:40 +0800

    🎉 feat: 图片支持放大展示

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,22 +1,41 @@
 import { useBoolean } from 'ahooks'
 import classNames from 'classnames'
 import { useNativeLazyLoading } from 'hooks/use-native-lazy-loading'
-import { ImgHTMLAttributes, memo, useCallback } from 'react'
+import mediumZoom from 'medium-zoom'
+import { ImgHTMLAttributes, memo, useCallback, useRef } from 'react'
 import { useInView } from 'react-intersection-observer'
 import { BlurImage } from './Blur'
 
+const ZOOM = mediumZoom()
+
 type Props = ImgHTMLAttributes<HTMLImageElement> & {
   hash?: string
+
+  /**
+   * 是否支持缩放
+   */
+  zoomable?: boolean
 }
 
-export const Image = memo<Props>(({ hash, className, onLoad, ...restProps }) => {
+export const Image = memo<Props>(({ hash, className, onLoad, onClick, zoomable = true, ...restProps }) => {
+  const zoomRef = useRef(ZOOM.clone({ background: 'white' }))
+
   const supportsLazyLoading = useNativeLazyLoading()
   const { ref, inView } = useInView({
     triggerOnce: true,
     skip: supportsLazyLoading !== false,
   })
+
   const [isLoaded, { setTrue: setImageLoaded }] = useBoolean(false)
   const [delayLoaded, { setTrue: setDelayLoaded }] = useBoolean(false)
+
+  const attachZoom = useCallback((ref) => {
+    if (zoomable) {
+      zoomRef.current.attach(ref)
+    } else {
+      zoomRef.current.detach(ref)
+    }
+  }, [zoomable])
 
   const onImageLoad = useCallback((e: any) => {
     setImageLoaded()
@@ -33,13 +52,20 @@ export const Image = memo<Props>(({ hash, className, onLoad, ...restProps }) => 
     }
   }, [hash, onLoad, setImageLoaded, setDelayLoaded])
 
+  const onImageClick = useCallback((e: any) => {
+    if (!zoomable && onClick) {
+      onClick(e)
+    }
+  }, [zoomable, onClick])
+
   return (
-    <div ref={ref} className={classNames('relative overflow-hidden', className)}>
+    <div ref={ref} className={classNames('relative overflow-hidden', className)} onClick={onImageClick}>
       {
         (inView || supportsLazyLoading) && (
           <img
             alt='img'
             {...restProps}
+            ref={attachZoom}
             loading='lazy'
             className='absolute inset-0 w-full h-full object-cover object-center m-0'
             onLoad={onImageLoad}
